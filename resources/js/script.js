b6d76a5337e0e7d2682fac06f6eb62fb722c34cc
Prince <k.prince1997@gmail.com>
2018-04-18 22:36:34 -0700

    First Commit

---
+++ resources/js/script.js
@@ -0,0 +1,21 @@
+// $ is called a seclector
+$(document).ready(function () {
+
+    $('.js--section-features').waypoint(function(direction) {
+
+        if (direction == "down") {
+
+            $('nav').addClass('sticky');
+
+        } else {
+
+            $('nav').removeClass('sticky');
+
+        }
+
+
+
+
+    });
+
+});
